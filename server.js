964d85568cd9bd6e716f405d80fc1e8d39917571
Finn Stobie <finnstobie0307@hotmail.com>
2022-09-12 19:45:03 +0300

    add slider captcha

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const saleItems = require('./routes/saleItems');
 const apiKeys = require('./routes/apiKeys');
 const currency = require('./routes/currency');
 const activities = require('./routes/activities');
+var sliderCaptcha = require('@slider-captcha/core');
 
 const PORT = process.env.PORT || 8000;
 
@@ -27,7 +28,33 @@ app.use(express.json());
 app.get('/', (_, res) => {
   res.send('pong');
 });
-app.use('/saleItems', saleItems);
-app.use('/apiKeys', apiKeys);
-app.use('/currency', currency);
-app.use('/activities', activities);
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.get('/captcha/create', function (req, res) {
+  console.log('req.session', req.session)
+  sliderCaptcha.create()
+    .then(function ({data, solution}) {
+      console.log('data', data)
+      req.session.captcha = solution;
+      req.session.save();
+      res.status(200).send(data);
+    });
+});
+
+app.post('/captcha/verify', function (req, res) {
+  sliderCaptcha.verify(req.session.captcha, req.body)
+    .then(function (verification) {
+      if (verification.result === 'success') {
+        req.session.token = verification.token;
+        req.session.save();
+      }
+      res.status(200).send(verification);
+    });
+});
